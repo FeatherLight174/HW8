d0ee2ad4acdd084a4685b49f5183d41d9ef0525e
FeatherLight174 <songyh2022@shanghaitech.edu.cn>
2024-06-11 05:29:13 -0700

    004

--- src/simulator.c
+++ src/simulator.c
@@ -17,7 +17,9 @@ status_t allocate_page(Process *process, addr_t address,
   size_t L1_page_num = (temp >> L2_BITS) & ((1<<L1_BITS)-1);
   size_t L2_page_num = temp & ((1<<L2_BITS)-1);
 
-  
+  if(main_memory->size<temp){
+    return ERROR;
+  }
 
   if(process->page_table.entries[L1_page_num].entries==NULL){
     process->page_table.entries[L1_page_num].entries = calloc(L2_PAGE_TABLE_SIZE, sizeof(PTE));
